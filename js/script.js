511838028fe293acd6fd3274cbabf9c74f97a745
Andrés Molinari <amolinarisalvo@gmail.com>
2023-08-03 22:50:46 -0300

    edición de página gymbrosRutinas

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
 $(document).ready(function() {
     const $daySelector = $('#daySelector');
     const $dayCells = $('.day-cell[data-day]');
-
-        
-    $daySelector.on('change', function() {
-      const selectedDay = $(this).val();
-      
+  
+    // Función para mostrar todas las celdas de día
+    function showAllDayCells() {
+      $dayCells.show();
+    }
+  
+    // Función para ocultar todas las celdas de día excepto el día seleccionado
+    function showSelectedDay(selectedDay) {
       $dayCells.hide();
       
       if (selectedDay === 'all') {
-        $dayCells.show();
+        showAllDayCells();
       } else {
         $dayCells.filter(`[data-day="${selectedDay}"]`).show();
+      }
+    }
+  
+    // Al cargar la página
+    const currentWindowWidth = $(window).width();
+    if (currentWindowWidth >= 768) {
+      showAllDayCells();
+    }
+  
+    // Al cambiar el día seleccionado
+    $daySelector.on('change', function() {
+      const selectedDay = $(this).val();
+      showSelectedDay(selectedDay);
+    });
+  
+    // Al cambiar el tamaño de la ventana
+    $(window).on('resize', function() {
+      const newWindowWidth = $(this).width();
+      const currentSelectedDay = $daySelector.val();
+  
+      if (newWindowWidth >= 768) {
+        showAllDayCells();
+      } else {
+        showSelectedDay(currentSelectedDay);
       }
     });
   });  
